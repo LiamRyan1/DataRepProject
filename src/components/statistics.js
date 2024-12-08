c73c8281246da0eb2aedf86f3b2dbc15fa6dc8f8
LiamRyan1 <G00416852@Atu.ie>
2024-12-08 12:16:57 +0000

    stats page

---
+++ src/components/statistics.js
@@ -0,0 +1,11 @@
+
+
+return(
+    <div style={{backgroundColor:'#b2d6d6' ,minHeight:'100vh'}}>
+        <h1>Statistics</h1>  
+        <div>
+        <p><strong>Total Books:<br/>Percentage of Books Read: <br/>Average Rating:<br/>Total Unread Books:<br/></strong></p>
+        </div>      
+    </div>
+);
+export default statistics;
