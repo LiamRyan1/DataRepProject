201f84efda14bfe5d38eae276fdbe449e3ae8923
LiamRyan1 <G00416852@Atu.ie>
2024-12-08 12:53:58 +0000

    do calcs after data fetched

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,36 +1,36 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-const Statistics= () => {
+const Statistics = () => {
     const [data, setData] = useState([]);
-    const [totalBooks,setTotalBooks] = useState(0);
-    const [percentRead,setPercentRead] = useState(0);
-    const [averageRating,setAverageRating] = useState(0);
-    const [totalUnRead,setTotalUnread] = useState(0);
+    const [totalBooks, setTotalBooks] = useState(0);
+    const [percentRead, setPercentRead] = useState(0);
+    const [averageRating, setAverageRating] = useState(0);
+    const [totalUnRead, setTotalUnread] = useState(0);
 
-    useEffect (() => {
-    axios.get("http://localhost:4000/api/books")
-    .then((response) => {
-        setData(response.data.books);
-    })
-    .catch((error) => {
-        console.error("Error reloading data:", error);
-    });
+    useEffect(() => {
+        axios.get("http://localhost:4000/api/books")
+            .then((response) => {
+                setData(response.data.books);
+                let total = data.length;
+                setTotalBooks(total);
+                const ReadBooks = data.filter(data => data.status !== "Read").length;
+                let Unread = totalBooks - ReadBooks;
+                setTotalUnread(Unread);
+                let Percent = ((ReadBooks / totalBooks) * 100);
+                setPercentRead(Percent);
+                const totalRating = 0;
+                for (let i = 0; i < data.length; i++) {
+                    totalRating += parseFloat(data[i].rating);
+                }
+                let AvgRating = (totalRating / totalBooks);
+                setAverageRating(AvgRating);
+
+            })
+            .catch((error) => {
+                console.error("Error reloading data:", error);
+            });
     }, []);
 
-     let total= data.length;
-     setTotalBooks(total);
-    const ReadBooks  =  data.filter(data => data.status !== "Read").length;
-    let Unread = totalBooks -ReadBooks;
-    setTotalUnread(Unread);
-    let Percent= ((ReadBooks/totalBooks)*100);
-    setPercentRead(Percent);
-    const totalRating = 0;
-    for(let i = 0 ; i < data.length; i++)
-    {
-        totalRating += parseFloat(data[i].rating);
-    }
-    let AvgRating = (totalRating/totalBooks);
-    setAverageRating(AvgRating);
 
     return (
         <div style={{ backgroundColor: '#b2d6d6', minHeight: '100vh' }}>
